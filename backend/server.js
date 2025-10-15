752eb7475fbd68223e18553a28d3f897981821ed
SriramKapaganti <koushiksriram3967@gmail.com>
2025-10-15 14:29:39 +0530

    backend cors change

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://knit-gilt.vercel.app/", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
